9c837fc6baee643aafad1cb89d574276c0754423
future2026123 <chasekwon1@gmail.com>
2026-03-18 11:52:45 +0900

    Update SW with clients.claim and auto-reload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,12 @@
-// Self-destruct: clear all caches and unregister
+// Self-destruct: clear all caches, claim clients, reload, and unregister
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+    clients.claim()
+    .then(() => caches.keys())
+    .then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+    .then(() => self.clients.matchAll())
+    .then((cls) => cls.forEach((c) => c.navigate(c.url)))
     .then(() => self.registration.unregister())
   );
 });
